a6405baaa16674b7e2412f684aee79f6d1199bb7
Felix Fontein <felix@fontein.de>
2021-01-09 23:33:49 +0100

    Forgot to remove removed local plugin from config.

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -47,7 +47,7 @@ AUTHOR = 'Ansible, Inc'
 # They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 # TEST: 'sphinxcontrib.fulltoc'
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'pygments_lexer', 'notfound.extension', 'sphinx_antsibull_ext']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'notfound.extension', 'sphinx_antsibull_ext']
 
 # Later on, add 'sphinx.ext.viewcode' to the list if you want to have
 # colorized code generated too for references.
